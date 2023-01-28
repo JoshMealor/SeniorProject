f7ab9c6f3419db5fc99a2171397a7cbad26e040b
CalvinBultz <bultzck@my.gvltec.edu>
2023-01-28 14:52:17 -0500

    Added the abillity to manage dues. Add, edit,  delete and view list of dues records. The same technique used for dues will be used on other tables. This technique involves leaving the models in the defaulte area>Models>DataLayer>TableModels unchanged. I have created purpose built viewmodels in the authenticated area that have a combination of two or more database tables. In the controllers, I just pulled matching records using foreign key Id's and built the viewmodel from that.  Also, for branching, I do believe I can merge this branch to master later. But only after it has been tested extensively. I might not do the branching for the other tables but I do want to give this a try and see how the branching works. Best Regards, -Calvin

--- SeniorProject/Areas/Authenticated/Controllers/ManagerController.cs
+++ SeniorProject/Areas/Authenticated/Controllers/ManagerController.cs
@@ -135,7 +135,7 @@ namespace SeniorProject.Areas.Authenticated.Controlllers
                 //Go back to the summary view of the members and set the content in the element below the table indcating an error
                 TempData["Change"] = "Sorry. No records found. Try again.";
                 //Redirect 
-                return RedirectToAction("ManageMembers", "Admin", new { area = "Authenticated" });
+                return RedirectToAction("ManageMembers", "Manager", new { area = "Authenticated" });
             }
 
             //A member was found
@@ -376,7 +376,347 @@ namespace SeniorProject.Areas.Authenticated.Controlllers
         }
 
 
-        
+
+
+
+
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> DuesSummary()
+        {
+            //Create a list of the viewModel to send
+            List<SeniorProject.Areas.Authenticated.Models.DuesViewModel> viewModelList = new List<Models.DuesViewModel>();
+
+            //Retrieve all dues table records
+            List<SeniorProject.Models.DataLayer.TableModels.Due> dues = _SPDBContext.Dues.ToList();
+            //Go through each due entry and add relavent info to the view model
+            foreach (var due in dues)
+            {
+                //Instantiate the single view model object
+                SeniorProject.Areas.Authenticated.Models.DuesViewModel viewModel = new Models.DuesViewModel();
+                //Go ahead and assign what data we can
+                viewModel.DueID = due.DueID;
+                viewModel.AmountDue= due.AmountDue;
+                viewModel.DateTimeDue= due.DateTimeDue;
+                viewModel.AmountPaid= due.AmountPaid;
+                viewModel.DateTimePaid= due.DateTimePaid;
+                viewModel.PaymentMethod= due.PaymentMethod;
+                viewModel.MemberID= due.MemberID;
+                //filter empty foreign keys to members table
+                if (due.MemberID != null && due.MemberID != 0)
+                {
+                    //There is a foreign key id in the field. Pull the related info
+                    SeniorProject.Models.DataLayer.TableModels.Member relatedRecord = _SPDBContext.Members.Find(due.MemberID);
+                    //Filter for none found
+                    if (relatedRecord != null)
+                    {
+                        //Assign info to the view model
+                        viewModel.MemberActiveStatus = relatedRecord.ActiveStatus;
+                        viewModel.MemberFirstName= relatedRecord.FirstName;
+                        viewModel.MemberLastName= relatedRecord.LastName;
+                    }
+                    else
+                    {
+                        //Assign empty strings if nothing was found
+                        viewModel.MemberActiveStatus = false;
+                        viewModel.MemberFirstName = relatedRecord.FirstName;
+                        viewModel.MemberLastName = relatedRecord.LastName;
+                    }
+                }
+                else
+                {
+                    //No foriegn key
+                    //Assign empty strings
+                    viewModel.MemberActiveStatus = false;
+                    viewModel.MemberFirstName = string.Empty;
+                    viewModel.MemberLastName = string.Empty;
+                }
+                //Add the single view model to the list of view models
+                viewModelList.Add(viewModel);
+            }
+
+            //Pass this list to the view 
+            return View(viewModelList);
+
+        }
+
+
+        //Add an attribute for routing. Http method: get
+        [HttpGet]
+        public IActionResult AddDue()
+        {
+            //Adjust the viewbag data in the view to reflect the action 
+            ViewBag.Action = "Add";
+            //Instantiate a new view model to pass to the view
+            SeniorProject.Areas.Authenticated.Models.DuesViewModel viewModel = new Models.DuesViewModel();
+            //Set all fields to defaults rather than null
+            viewModel.DueID = 0;
+            viewModel.AmountDue= 0;
+            viewModel.DateTimeDue = DateTime.Now;
+            viewModel.AmountPaid = 0;
+            viewModel.DateTimePaid = DateTime.Now;
+            viewModel.PaymentMethod = string.Empty;
+            viewModel.MemberID= 0;
+            viewModel.MemberActiveStatus= false;
+            viewModel.MemberFirstName = string.Empty;
+            viewModel.MemberLastName = string.Empty;
+
+            //There is a search feature on the view with javascript for searching member names
+            //Pass empty view model
+            return View("AddEditDues", viewModel);
+        }
+
+
+        //Add an attribute for routing. http method: get
+        [HttpGet]
+        public async Task<IActionResult> EditDueAsync(int Id)
+        {
+            //Adjust the viewbag data in the view to reflect the action. The view for add and edit is multiuse.
+            ViewBag.Action = "Edit";
+            //Instantiate the view model
+            SeniorProject.Areas.Authenticated.Models.DuesViewModel viewModel = new Models.DuesViewModel();
+
+            //Retrieve the records info assocaited with the primary key Id passed in
+            SeniorProject.Models.DataLayer.TableModels.Due record = _SPDBContext.Dues.Find(Id);
+            //Filter for no record found
+            if (record == null)
+            {
+                //No record found
+                //Go back to the summary view and set the content in the element below the table indcating an error
+                TempData["Change"] = "Sorry. No records found. Try again.";
+                //Redirect 
+                return RedirectToAction("DuesSummary", "Manager", new { area = "Authenticated" });
+            }
+
+            //A record was found
+            //Go ahead and assign what data we can
+            viewModel.DueID= record.DueID;
+            viewModel.AmountDue= record.AmountDue;
+            viewModel.DateTimeDue= record.DateTimeDue;
+            viewModel.AmountPaid= record.AmountPaid;
+            viewModel.DateTimePaid= record.DateTimePaid;
+            viewModel.PaymentMethod= record.PaymentMethod;
+            viewModel.MemberID= record.MemberID;
+            
+
+            //Filter for empty foreign key
+            if (record.MemberID != null && record.MemberID != 0)
+            {
+                //There is a foreign key. Get that record
+                SeniorProject.Models.DataLayer.TableModels.Member relatedRecord = _SPDBContext.Members.Find(record.MemberID);
+                //Filter for no record found
+                if (relatedRecord != null)
+                {
+                    //Assign info to the view model
+                    viewModel.MemberActiveStatus = relatedRecord.ActiveStatus;
+                    viewModel.MemberFirstName= relatedRecord.FirstName;
+                    viewModel.MemberLastName= relatedRecord.LastName;
+                }
+                else
+                {
+                    //No record found
+                    //Assign defaults
+                    viewModel.MemberActiveStatus = false;
+                    viewModel.MemberFirstName = string.Empty;
+                    viewModel.MemberLastName= string.Empty;
+                }
+            }
+            else
+            {
+                //No foreign key
+                //Assign defaults
+                viewModel.MemberActiveStatus = false;
+                viewModel.MemberFirstName = string.Empty;
+                viewModel.MemberLastName = string.Empty;
+            }
+            return View("AddEditDues", viewModel);
+        }
+
+
+        //Add an attribute for routing. http method: post 
+        [HttpPost]
+        public async Task<IActionResult> AddEditDuesAsync(SeniorProject.Areas.Authenticated.Models.DuesViewModel viewModel)
+        {
+            //This method is used for both adding and editing:  post methods
+
+            //Preform built in validation for the model
+            if (ModelState.IsValid)
+            {
+                //Determine if adding or editing based on the Id value passed in
+                if (viewModel.DueID == 0)
+                {
+                    //Adding
+
+                    //Instantiate a table record model 
+                    SeniorProject.Models.DataLayer.TableModels.Due record = new SeniorProject.Models.DataLayer.TableModels.Due();
+
+                    //Update all the record fields
+                    record.AmountDue= viewModel.AmountDue;
+                    record.DateTimeDue= viewModel.DateTimeDue.GetValueOrDefault();
+                    record.AmountPaid= viewModel.AmountPaid;
+                    record.DateTimePaid= viewModel.DateTimePaid.GetValueOrDefault();
+                    record.PaymentMethod = viewModel.PaymentMethod;
+                   
+
+                    //Retrive a matching related record
+                    SeniorProject.Models.DataLayer.TableModels.Member relatedRecord = _SPDBContext.Members.Find(viewModel.MemberID);
+                    if (relatedRecord != null)
+                    {
+                        //Match found
+                        //Add the now validated foriegn key to the record
+                        record.MemberID = relatedRecord.MemberID;
+                        //update anything  in the related record that can be changed from this view/access
+                        //This is only the active status. Restrict name change to another view.
+                        bool changemade = false;
+                        if (relatedRecord.ActiveStatus != viewModel.MemberActiveStatus)
+                        {
+                            relatedRecord.ActiveStatus = viewModel.MemberActiveStatus.GetValueOrDefault();
+                            changemade = true;
+                        }
+                        //Determing it the related table needs to be updated                       
+                        if (changemade)
+                        {
+                            //Update the related table
+                            _SPDBContext.Members.Update(relatedRecord);
+                            _SPDBContext.SaveChanges();
+                            
+                        }
+
+                    }
+                    else
+                    {
+                        //Return an error to the add view
+                        ModelState.AddModelError("No match", "Member not found. ");
+                        return View("AddEditDues", viewModel);
+                    }
+
+                    //All filters did not produce an error                    
+                    //Set the tempdata to add success
+                    TempData["Change"] = "You successfully added a due entry.";
+                    //Add the record 
+                    _SPDBContext.Dues.Add(record);
+                    //Update the database
+                    _SPDBContext.SaveChanges();
+                    //Return to the summary table
+                    return RedirectToAction("DuesSummary", "Manager", new { area = "Authenticated" });
+                }
+                else
+                {
+                    //Updating
+
+                    //Instantiate a table record model 
+                    SeniorProject.Models.DataLayer.TableModels.Due record = new SeniorProject.Models.DataLayer.TableModels.Due();
+
+                    //Update all the record fields
+                    record.DueID= viewModel.DueID;
+                    record.AmountDue = viewModel.AmountDue;
+                    record.DateTimeDue = viewModel.DateTimeDue.GetValueOrDefault();
+                    record.AmountPaid = viewModel.AmountPaid;
+                    record.DateTimePaid = viewModel.DateTimePaid.GetValueOrDefault();
+                    record.PaymentMethod = viewModel.PaymentMethod;
+
+                    //Retrive a matching related record
+                    SeniorProject.Models.DataLayer.TableModels.Member relatedRecord = _SPDBContext.Members.Find(viewModel.MemberID);
+                    if (relatedRecord != null)
+                    {
+                        //Match found
+                        //Add the now validated foriegn key to the record
+                        record.MemberID = relatedRecord.MemberID;
+                        //update anything that can be changed form this view/access
+                        //This is only the active status. Restrict name change to another view.
+                        bool changemade = false;
+                        if (relatedRecord.ActiveStatus != viewModel.MemberActiveStatus)
+                        {
+                            relatedRecord.ActiveStatus = viewModel.MemberActiveStatus.GetValueOrDefault();
+                            changemade = true;
+                        }
+                        //Determing it the related table needs to be updated                       
+                        if (changemade)
+                        {
+                            //Update the related table
+                            _SPDBContext.Members.Update(relatedRecord);
+                            _SPDBContext.SaveChanges();
+
+                        }
+
+                    }
+                    else
+                    {
+                        //Return an error to the add view
+                        ModelState.AddModelError("No match", "Member not found. ");
+                        return View("AddEditDues", viewModel);
+                    }
+
+                    //All filters did not produce an error                    
+                    //Set the tempdata to update success
+                    TempData["Change"] = "You successfully updated a due record.";
+                    //Update the record in the table
+                    _SPDBContext.Dues.Update(record);
+                    //Update the database
+                    _SPDBContext.SaveChanges();
+                    //Return to the summary table
+                    return RedirectToAction("DuesSummary", "Manager", new { area = "Authenticated" });
+
+                }
+            }
+            else
+            {
+                //Model errors should add automatically
+                //Return back to the addedit view
+                return View("AddEditDues", viewModel);
+            }
+        }
+
+
+        //Add an attribute for routing. http method: get
+        [HttpGet]
+        public IActionResult DeleteDue(int Id)
+        {
+            //Retrieve the record from the table in the database
+            SeniorProject.Models.DataLayer.TableModels.Due record = _SPDBContext.Dues.Find(Id);
+            //Filter for none found
+            if (record == null)
+            {
+                //None found
+                //Return an error
+                //Set the tempdata to delete fail
+                TempData["Change"] = "Failed to remove due entry Id: " + Id;
+                //Redirect to the summary page
+                return RedirectToAction("DuesSummary", "Manager", new { area = "Authenticated" });
+            }
+
+            //The record was found
+            //Use the record found as the parameter entity to remove
+            _SPDBContext.Dues.Remove(record);
+            //Commit the changes to the database using the dbcontext savechanges method
+            _SPDBContext.SaveChanges();
+            //Set the tempdata to delete success
+            TempData["Change"] = "You successfully removed due reocord ID " + record.DueID;
+            //Reditect the summary page
+            return RedirectToAction("DuesSummary", "Manager", new { area = "Authenticated" });
+        }
+
+
+
+        //The add or edit will have a search box for members records, search by names
+        //There will be a javascript ajax post in this controller that returns the match or no results found
+        //When match is found a btn javascipt fills in the form with the matching member info
+        //User then has to hit submit on form to actually add or edit the due record
+        //Ensure authentication on this works
+        //The dues due date can be javascript as well once the date payed changes.
+
+
+
+
+
+
+
+
+
+
 
     }
 }
